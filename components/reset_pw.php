5b9411d5fa7f897bb8cb38864d5810b55b762a79
LuxorUWU <lucjan.lubomski@gmail.com>
2024-10-31 15:09:05 +0100

    copied php from login and adapted to fit into the password change field

--- components/reset_pw.php
+++ components/reset_pw.php
@@ -1,4 +1,44 @@
 <!-- Password reset api handling -->
+<?php
+  if(!empty($_POST)) {
+    error_log("Form submitted. Data: " . print_r($_POST, true));
+    $password = $_POST['new_pwd'];
+    $password_confirm = $_POST['new_pwd_r'];
+
+    if ($password !== $password_confirm) {
+      echo "Password nicht gleich";
+      exit();
+    }
+  
+    $data = array(
+      "paddword" => $password
+    );
+    $json_data = json_encode($data);
+  
+    $reset = curl_init("https://tomatenbot.com/api/gaminghotel/passwordreset");
+    curl_setopt($reset, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($reset, CURLOPT_POST, true);
+    curl_setopt($reset, CURLOPT_POSTFIELDS, $json_data);
+    curl_setopt($reset, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+  
+    $response = curl_exec($reset);
+    $res_data = json_decode($response, false);
+    if ($res_data === false) {
+      echo 'Curl error: ' . curl_error($reset);
+    } else {
+      if($res_data->ok === true){
+        setcookie("loginToken", $res_data->token, time() + 60 * 60 * 24 * 3, "/", "localhost");
+        curl_close($reset);
+        header("Location: /index.php");
+        exit();
+      }
+    }
+
+    error_log(print_r($res_data, true));
+
+    curl_close($reset);
+  }
+?>
 
 <section id="resetSection" class="flex-column form-container" hidden>
   <form class="flex-column" method="POST">
