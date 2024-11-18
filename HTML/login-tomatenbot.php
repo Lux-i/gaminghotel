067d890a684a42c4c74b22161a08ad7a3bd1de3e
LuxorUWU <s-s@s.com>
2024-11-18 21:03:59 +0100

    comment error fix, deleted html section

--- HTML/login-tomatenbot.php
+++ HTML/login-tomatenbot.php
@@ -117,23 +117,4 @@ $username = "Log in";
     
         curl_close($getData);
     }
-
-//PASSWORD RESET
-
-/*<section class="data-splitter">
-                <button id="resetButton" class="button-cream border-none" onclick="showReset()">
-                  Reset Password
-                </button>
-                <?php
-                  include(__DIR__ . '/../components/reset_pw.php');
-                ?>
-</section>
-
-<script src="../JS/me.js"></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-      crossorigin="anonymous"></script>
-
 ?>
-*/
