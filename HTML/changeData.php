12f8de7eac8895fa490b082fe3104c8fc61c3d68
LuxorUWU <lucjan.lubomski@gmail.com>
2024-12-05 13:48:37 +0100

    started work on the data change workflow

--- HTML/changeData.php
+++ HTML/changeData.php
@@ -1,20 +1,56 @@
 <?php
 session_start();
 if (isset($_POST)) {
-    $_SESSION['anrede'] = !empty($_POST['gender']) ? $_POST['gender'] : $_SESSION['anrede'];
-    $_SESSION['username'] = !empty($_POST['username']) ? $_POST['username'] : $_SESSION['username'];
-    $_SESSION['vorname'] = !empty($_POST['vorname']) ? $_POST['vorname'] : $_SESSION['vorname'];
-    $_SESSION['nachname'] = !empty($_POST['nachname']) ? $_POST['nachname'] : $_SESSION['nachname'];
-    $_SESSION['email'] = !empty($_POST['email']) ? $_POST['email'] : $_SESSION['email'];
     if(!empty($_POST['pwd'])){
         if($_POST['pwd'] == $_SESSION['pwd'] && $_POST['new_pwd'] == $_POST['new_pwd_again']){
-            //$_SESSION['pwd'] = password_hash($_POST['new_pwd'], PASSWORD_DEFAULT);
-            $_SESSION['pwd'] = $_POST['new_pwd'];
+            /*
+                pw needs to be hashed again and a update sql statement needs to be executed
+            */
         } else {
             header('Location: change_pwd.php?error=wrong input');
             die();
         }
     }
 }
+
+require_once('../components/dbaccess.php');
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if(!empty($_POST)){
+    /*
+        sql statement uses id, which isn't stored
+        either store the id for now or wait till the token auth system is finished
+    */
+    $sql = "UPDATE users 
+            SET anrede = ?, name = ?, nachname = ?, username = ?, email = ?
+            WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+
+    $anrede = $_POST['anrede'];
+    $username = $_POST['username'];
+    $vorname = ucfirst($_POST['vorname']);
+    $nachname = ucfirst($_POST['nachname']);
+    $email = $_POST['email'];
+    $pwd = password_hash($_POST['pwd'], PASSWORD_ARGON2ID);
+    $rolle = "user";
+
+    $stmt->bind_param("sssssi", /*fill user data*/);
+
+    if ($stmt->execute()) {
+      echo '<div class="alert alert-success mt-3 center-txt w-25">New record created successfully!</div>';
+    } else {
+      echo '<div class="alert alert-danger mt-3 center-txt w-25" role="alert">Error: ' . $stmt->error . '</div>';
+    }
+
+    $conn->close();
+}
+
 header('Location: me.php');
 ?>
