333d4eacfa6848810769829f9577891a790c3ffb
LuxorUWU <s-s@s.com>
2024-12-17 21:02:06 +0100

    formatting changes i think

--- HTML/change_booking_status.php
+++ HTML/change_booking_status.php
@@ -1,16 +1,16 @@
 <?php
-if(!empty($_POST)){
+if (!empty($_POST)) {
     session_start();
 
     if ($_SESSION["logged"] == true) {
         require_once '../components/db_utils.php';
         $conn = connectDB();
         if (validateToken($conn)) {
-            if($_POST["change_status"] == 1){
+            if ($_POST["change_status"] == 1) {
                 $sql = "UPDATE bookings SET status = 'storniert' WHERE id = ?";
                 $stmt = $conn->prepare($sql);
                 $stmt->bind_param("i", $_POST["id"]);
-            }elseif($_POST["change_status"] == 2){
+            } elseif ($_POST["change_status"] == 2) {
                 $sql = "UPDATE bookings SET status = 'bestätigt' WHERE id =?";
                 $stmt = $conn->prepare($sql);
                 echo $_POST["id"];
@@ -29,7 +29,7 @@ if(!empty($_POST)){
     }
 
 
-}else {
+} else {
     header('Location: ' . $_SERVER['HTTP_REFERER']);
     die();
 }
