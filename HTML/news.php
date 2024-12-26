8ea6164074d0a85bd18ea15d19367477e1995e69
LuxorUWU <s-s@s.com>
2024-12-26 16:35:35 +0100

    rewrote to use db_utils's connectBD and removed token validation (news should be readable for logged off users as well)

--- HTML/news.php
+++ HTML/news.php
@@ -1,52 +1,53 @@
 <?php
-  include(__DIR__ . '/../components/header.php');
-  include(__DIR__ . '/../components/nav.php');
-  include(__DIR__ . '/news-articles.php');
-  require_once('../components/dbaccess.php');
-  require_once('../components/db_utils.php');
-  $conn = new mysqli($servername, $username, $password, $dbname);
+include(__DIR__ . '/../components/header.php');
+include(__DIR__ . '/../components/nav.php');
+require_once('../components/db_utils.php');
+
+$conn = connectDB();
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-if(validateToken($conn)){
-  $sql = "SELECT * FROM news_articles ORDER BY id DESC";
-  $stmt = $conn->prepare($sql);
-  if($stmt->execute()){
-    $result = $stmt->get_result();
-    $rownum = 0; 
-    $news = array();
-    while($row = $result->fetch_assoc()){
-      $news[$rownum++] = $row;
-    }
+
+$sql = "SELECT * FROM news_articles ORDER BY id DESC";
+$stmt = $conn->prepare($sql);
+if ($stmt->execute()) {
+  $result = $stmt->get_result();
+  $rownum = 0;
+  $news = array();
+  while ($row = $result->fetch_assoc()) {
+    $news[$rownum++] = $row;
   }
 }
+
 closeConnection($conn);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Buchen | Göppel & Göppel Hotels</title>
-    <?php
-      include(__DIR__ . '/../components/main_style.php');
-    ?>
-    <link rel="stylesheet" href="../css/news.css">
-    <link rel="stylesheet" href="/CSS/login-register.css" />
-  </head>
-  <body class="ghostwhite">
-    <?php 
-      echo '<h1 class="text-center">News</h1>';
-      include(__DIR__ . '/../components/in_work.php');
-    ?>
-    <?php if ($_SESSION['logged'] == true) : ?> 
-      <?php if($user['rolle'] == "admin") : ?>
-          
-        <main class="d-flex flex-row">
-          <section class="login-window mb-3 w-75">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Buchen | Göppel & Göppel Hotels</title>
+  <?php
+  include(__DIR__ . '/../components/main_style.php');
+  ?>
+  <link rel="stylesheet" href="../css/news.css">
+  <link rel="stylesheet" href="/CSS/login-register.css" />
+</head>
+
+<body class="ghostwhite">
+  <?php
+  echo '<h1 class="text-center">News</h1>';
+  include(__DIR__ . '/../components/in_work.php');
+  ?>
+  <?php if ($_SESSION['logged'] == true): ?>
+    <?php if ($user['rolle'] == "admin"): ?>
+
+      <main class="d-flex flex-row">
+        <section class="login-window mb-3 w-75">
           <form action="/components/upload.php" method="POST" enctype="multipart/form-data">
             <h2 class="border-bottom-black">Neuer Artikel</h2>
             <div class="mb-4">
@@ -59,37 +60,38 @@ closeConnection($conn);
             </div>
             <div class="mb-4">
               <label for="content">Inhalt</label>
-              <textarea class="inputfield container-md form-control" name="content" id="content" rows="4" cols="50"></textarea>
+              <textarea class="inputfield container-md form-control" name="content" id="content" rows="4"
+                cols="50"></textarea>
             </div>
             <div class="d-flex justify-content-center">
               <input class="submit_button w-25" type="submit" name="submit" value="Veröffentlichen" />
             </div>
           </form>
-          </section>
-        </main>
+        </section>
+      </main>
 
-      <?php endif; ?>
     <?php endif; ?>
+  <?php endif; ?>
 
-    <?php foreach ($news as $article) : ?>
-      <a href="article.php?id=<?= urlencode($article['id']) ?>"
+  <?php foreach ($news as $article): ?>
+    <a href="article.php?id=<?= urlencode($article['id']) ?>"
       class="d-block text-decoration-none text-reset container w-75 news-pre text-start p-0">
-        <section class="txt-container">
-          <h2><?= htmlspecialchars($article["title"]) ?></h2>
-          <p><?= htmlspecialchars($article["sub"]) ?></p>
-        </section>
-        <section class="img-container img-thumbnail">
-          <img src="<?= htmlspecialchars($article["img_path"])?>"/>
-        </section>
-      </a>
-    <?php endforeach?>
-    
-    <?php  
-      include(__DIR__ . '/../components/footer.php');
-    ?>
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-      crossorigin="anonymous"></script>
-  </body>
+      <section class="txt-container">
+        <h2><?= htmlspecialchars($article["title"]) ?></h2>
+        <p><?= htmlspecialchars($article["sub"]) ?></p>
+      </section>
+      <section class="img-container img-thumbnail">
+        <img src="<?= htmlspecialchars($article["img_path"]) ?>" />
+      </section>
+    </a>
+  <?php endforeach ?>
+
+  <?php
+  include(__DIR__ . '/../components/footer.php');
+  ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</body>
+
 </html>
