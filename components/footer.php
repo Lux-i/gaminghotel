b603bf6ae604836e9709806c8d7cbaeb5831f4a1
LuxorUWU <s-s@s.com>
2025-01-08 18:37:03 +0100

    updated favicon
    removed empty footer links
    
    changed logo color
    
    updated webmanifest

--- components/footer.php
+++ components/footer.php
@@ -3,19 +3,13 @@
     <section class="footer-column flex-column">
       <a href="/HTML/impressum.php" class="footer-link">Impressum</a>
       <a href="/HTML/hilfe.php" class="footer-link">Hilfe</a>
-      <a href="/sitemap.html" class="footer-link">Sitemap</a>
-    </section>
-    <section class="footer-column flex-column">
-      <a href="/index.php" class="footer-link">Bsp. Link</a>
-      <a href="/index.php" class="footer-link">Bsp. Link</a>
-      <a href="/index.php" class="footer-link">Bsp. Link</a>
+      <a href="/HTML/news.php" class="footer-link">News</a>
     </section>
   </section>
   <section class="footer-group flex-row flex-end">
     <section class="footer-column flex-column">
+      <a href="/sitemap.html" class="footer-link">Sitemap</a>
       <a href="/sitemap.xml" class="footer-link">Sitemap (xml)</a>
-      <a href="/index.php" class="footer-link">Bsp. Link</a>
-      <a href="/index.php" class="footer-link">Bsp. Link</a>
     </section>
   </section>
 </footer>
--- components/main_style.php
+++ components/main_style.php
@@ -1,11 +1,9 @@
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
-<link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-      crossorigin="anonymous" />
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<link rel="manifest" href="/site.webmanifest" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="/CSS/main.css" />
